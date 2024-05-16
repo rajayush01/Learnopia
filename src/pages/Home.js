3f166ef3ddeac4bcbe3cde9497e0d021e8e6c3a1
Ayush Raj <119657235+rajayush01@users.noreply.github.com>
2024-05-16 20:51:59 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,25 @@
 import React from 'react'
 import back from "../assets/frame.png"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const Home = ({ isLoggedIn }) => {
   return (
     <div className='flex justify-center items-center h-full'>
       <img src={back} alt="back" width={600} height={40} loading="lazy" />
       <div className='absolute mb-20'>
-          <div className='text-white text-3xl mx-4'>Welcome to Learnopia</div>
-          <br/>
-          <br/>
-          <div className='flex flex-row gap-5'>
-
-          <button className='border-4 rounded-2xl outline-white text-2xl text-cyan-500 bg-transparent hover:bg-cyan-500 hover:text-white transition-colors duration-300 transform hover:scale-90'>
-          <Link to="/Login">Students</Link>
+        <div className='text-white text-4xl mx-8'>Welcome to Learnopia</div>
+        <br />
+        <br />
+        <div className='flex flex-row gap-5'>
+          <button className='border-4 rounded-2xl outline-white text-2xl text-cyan-500 bg-transparent hover:bg-cyan-500 hover:text-white transition-colors duration-300 transform hover:scale-90 px-6 py-3'>
+            <Link to="/Login">Students</Link>
           </button>
-          <button className='border-4 rounded-2xl outline-white text-2xl text-cyan-500 bg-transparent hover:bg-cyan-500 hover:text-white transition-colors duration-300 transform hover:scale-90'>
-          <Link to="/Signup">Become an Educator</Link>
+          <button className='border-4 rounded-2xl outline-white text-2xl text-cyan-500 bg-transparent hover:bg-cyan-500 hover:text-white transition-colors duration-300 transform hover:scale-90 px-6 py-3'>
+            <Link to="/Signup">Become an Educator</Link>
           </button>
-        </div>
-
         </div>
       </div>
-    
+    </div>
   )
 }
 
